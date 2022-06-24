ab29eaf0cba5eeb5b1d75435f8c7bfa7fdd7a37d
Rodrigo Orozco <orozcorodi8@gmail.com>
2022-06-24 03:52:54 -0300

    titulo, efectos y algunos botones agregados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,16 @@ const Home = () => {
   return (
     <>
       <Hero />
+      <div className="titulo-cards">
+        <h5>Descubre nuestros</h5>
+        <h3>NUEVOS LANZAMIENTOS</h3>
+      </div>
       <Card tituloCards={"Obras Destacadas"} />
       <Card tituloCards={"Colecciones"} />
+      <div className="botonesHome">
+        <button className="btn-token">TOKEN</button>
+        <button className="btn-token">IR AL BLOG</button>
+      </div>
     </>
   );
 };
